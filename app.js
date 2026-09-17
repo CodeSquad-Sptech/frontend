de5d6e67ba8d659a1c1a793564ac233dde9e2924
Jay27032000 <julia.yamasaki@sptech.school>
2026-09-17 18:40:55 -0300

    primeiro ajuste na webdataviz

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
-// var ambiente_processo = 'producao';
-var ambiente_processo = 'desenvolvimento';
 
-var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
-// Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
-// A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
+// const ambiente_processo = 'producao';
+   const ambiente_processo = 'desenvolvimento';
 
-require("dotenv").config({ path: caminho_env });
+    const caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev'; 
 
-var express = require("express");
-var cors = require("cors");
-var path = require("path");
-var PORTA_APP = process.env.APP_PORT;
-var HOST_APP = process.env.APP_HOST;
+    require("dotenv").config({ path: caminho_env });
 
-var app = express();
+    const PORTA_APP = process.env.APP_PORT;
+    const HOST_APP = process.env.APP_HOST;
 
-var indexRouter = require("./src/routes/index");
-var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
+    const express = require("express");
+    const cors = require("cors");
+    const path = require("path");
+    const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.static(path.join(__dirname, "public")));
+    app.use(cors());
 
-app.use(cors());
 
-app.use("/", indexRouter);
-app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
+const cadastroRouter = require("./src/routes/cadastro_routes.js");
+const codigoRouter = require("./src/routes/codigo_routes.js");
+const convidadoRouter = require("./src/routes/convidado_routes.js");
+const economiaRouter = require("./src/routes/economia_routes.js");
+const listaRouter = require("./src/routes/lista_routes.js");
+const indexRouter = require("./src/routes/login_routes.js");
+
+app.use("/usuarios",  cadastroRouter);
+app.use("/codigo",    codigoRouter);
+app.use("/convidado", convidadoRouter);
+app.use("/economia",  economiaRouter);
+app.use("/lista",     listaRouter);
+app.use("/",          indexRouter);
+
 
 app.listen(PORTA_APP, function () {
     console.log(`
-    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
+     ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
     ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
     ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
     ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
     #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
     ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
     ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
-    \n\n\n                                                                                                 
-    Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
-    Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
-    \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
-    \tSe .:producao:. você está se conectando ao banco remoto. \n\n
-    \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
+    
+    ---------------------------------------------------
+    Servidor: http://${HOST_APP}:${PORTA_APP} 
+    Ambiente: ${process.env.AMBIENTE_PROCESSO}
+    ---------------------------------------------------
+    `);
 });
